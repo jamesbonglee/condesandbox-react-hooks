2998583c85d7bc1af09d09076c3aaa1db55eddf1
Jae Bong LEE <jamesbsec@gmail.com>
2021-04-03 21:46:48 +0900

    Difference between using and not Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>increment</button>
+      <button onClick={decrementItem}>decrement</button>
     </div>
   );
+};
+// 훅사용한 함수형 과 스테이트사용하는 클래스형 차이
+class ugly extends React.Component {
+  state = {
+    item: 1
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="ugly">
+        <h1>Hello {item}</h1>
+        <h2>ugly happen!</h2>
+        <button onClick={this.incrementItem}>increment</button>
+        <button onClick={this.decrementItem}>decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
 }
+export default App;
