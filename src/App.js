00944ea5d7eb1a16053b25658204b68a1e163525
Jae Bong LEE <jamesbsec@gmail.com>
2021-04-04 16:31:27 +0900

    hook useInput

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>increment</button>
-      <button onClick={decrementItem}>decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="name" {...name} />
+      {/* <input placeholder="name" value={name.value} onChange={name.onChange} 코드와 동일 /> */}
     </div>
   );
 };
-// 훅사용한 함수형 과 스테이트사용하는 클래스형 차이
-class ugly extends React.Component {
-  state = {
-    item: 1
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="ugly">
-        <h1>Hello {item}</h1>
-        <h2>ugly happen!</h2>
-        <button onClick={this.incrementItem}>increment</button>
-        <button onClick={this.decrementItem}>decrement</button>
-      </div>
-    );
-  }
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
-}
+
 export default App;
