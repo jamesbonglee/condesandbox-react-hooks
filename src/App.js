560ed3c1c758ef743ddd583a92bfdfe111479f71
Jae Bong LEE <jamesbsec@gmail.com>
2021-04-04 17:18:23 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
 
-    let willUpdate = true;
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
 
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
 
-    if (willUpdate) {
-      setValue(value);
-    }
+  return {
+    currentIndex: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-
-  return { value, onChange };
 };
 
+// 맵의 매개변수 두번째는 인덱스이다
+// const array1 = [1, 4, 9, 16];
+// const map1 = array1.map((x,i) => console.log("test",x*2,i));
+// // "test" 2 0
+// // "test" 8 1
+// // "test" 18 2
+// // "test" 32 3
+
 const App = () => {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
+  const { currentIndex, changeItem } = useTabs(0, content);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="name" {...name} />
-      {/* <input placeholder="name" value={name.value} onChange={name.onChange} 코드와 동일 /> */}
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+
+      <div>{currentIndex.content}</div>
     </div>
   );
 };
 
 export default App;
+
+// index는 0또는1이 되어야하고 모든 버튼은 온클릭(이벤트)를가진다.
+// 누군가 클릭하면 우리는 index가 무엇이든지 상관없이 changeItem(index)을 실행할것이고
+// changeItem의 index는 index안에 있는 값인0또는 1로 바꿔준다. 그건 setCurrentIndex의 item을 바꿔줄것이다.
+// 이것은 State를 바꿔줄거다. 그래서 currentIndex의 currentIndex를 바꿔줄것이고 모든 것을 새로고침 하는것이다.
+// 그럼 이제 클릭하는 곳으로 가게 될것이다
